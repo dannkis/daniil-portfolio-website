7c9043e9ee297eeadba8c65e663396f6ad9d9abd
Daniil Hristoforov Zhelyazkov <62340093+dannkis@users.noreply.github.com>
2026-01-12 21:47:53 +0000

    Removed ProjectCard (overkill for such a small component).
    - Added logic for different states whenever a project is selected.

--- portfolio/components/sections/ProjectsSection.tsx
+++ portfolio/components/sections/ProjectsSection.tsx
@@ -1,22 +1,51 @@
 "use client";
+import { useState } from "react";
 import { projects } from "@/data/projects";
-import ProjectCard from "../projects/ProjectCard";
+
+// Deriving the ID type to avoid invalid ID and it is also easier for future refactoring.
+type ProjectID = (typeof projects)[number]["id"];
 
 export default function ProjectsSection() {
+  const [expandedProjectID, setExpandedProjectID] = useState<ProjectID | null>(
+    null,
+  );
+  const expandedProject = projects.find((p) => p.id === expandedProjectID);
+  console.log(expandedProject?.name);
   return (
     <div className="box-container col-span-2 row-span-3" id="projects">
       <div className="box-subcontainer flex flex-col">
         <h1 className="mb-6">Projects</h1>
         <div className="grid h-full grid-cols-3 items-center gap-x-6 gap-y-10 overflow-scroll">
           {projects.map((project) => (
-            <ProjectCard
+            <button
+              type="button"
               key={project.id}
-              name={project.name}
-              image={project.image}
-            />
+              className="flex flex-col items-center"
+              onClick={() => setExpandedProjectID(project.id)}
+              aria-expanded={project.id === expandedProjectID}
+            >
+              <div>
+                <img
+                  src={project.image.src}
+                  alt={project.image.alt}
+                  draggable={false}
+                  className="transition hover:scale-[1.02]"
+                />
+              </div>
+              <p className="subheading">{project.name}</p>
+              <p className="subheading">{expandedProjectID}</p>
+            </button>
           ))}
         </div>
       </div>
     </div>
   );
+}
+
+export function ProjectsGridView() {
+  return <></>;
+}
+
+export function ProjectExpandedView(expandedProjectID: ProjectID) {
+  return <></>;
 }
